2d36e4ab8ceabe5e4bc5fc54b0713b0e85056b50
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-26 03:09:27 +0000

    mouse pointer react - 2-2

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -8,7 +8,9 @@ export default function AppXY() {
       className='container' 
       onPointerMove={(e) => {
         console.log(e.clientX, e.clientY);  
-        setPosition({ x: e.clientX, y: e.clientY });
+        // setPosition({ x: e.clientX, y: e.clientY });
+        // 만약 수평으로만 이동이 가능하다면?
+        setPosition(prev => ({ x: e.clientX, y: prev.y }));
         }}
       >
       <div 
